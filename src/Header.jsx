1894877fa98abb890fac3ffbf0375d7672eab3d0
karim <karimisaac85@gmail.com>
2024-05-27 13:53:21 +0300

    nav items works but button not shown

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,40 +12,41 @@ import {Link} from "react-scroll";
 
     return(    
 
-<nav className="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target bg-dark" style={{position:"fixed"}}>  <div className="container ">
+<nav className="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target"  id="ftco-navbar" >  
+
    
       <a className="navbar-brand" href="#">Karim</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className="collapse navbar-collapse" id="ftco-nav">
+      <ul className="navbar-nav nav ml-auto">
 
         <li className="nav-item">
-          <Link to="home" spy={true} smooth={true} offset={50} duration={500} className="nav-link " aria-current="page" href="#">Home</Link>
+          <Link to="home" spy={true} smooth={true} offset={50} duration={500} className="nav-link " aria-current="page" href="#"><span>Home</span></Link>
         </li>
 
         <li className="nav-item">
-          <Link to="about-section" spy={true} smooth={true} offset={50} duration={500} a className="nav-link" href="#">About</Link>
+          <Link to="about-section" spy={true} smooth={true} offset={50} duration={500} a className="nav-link" href="#"><span>About</span></Link>
         </li>
     
         <li className="nav-item">
-          <Link to="resume-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" >Resume</Link>
+          <Link to="resume-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" ><span>Resume</span></Link>
         </li>
         <li className="nav-item">
-          <Link to="skills-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" >Skills</Link>
+          <Link to="skills-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" ><span>Skills</span></Link>
         </li>
         <li className="nav-item">
-          <Link to="projects-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" >Projects</Link>
+          <Link to="projects-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" ><span>Projects</span></Link>
         </li>        
         
         <li className="nav-item">
-          <Link to="contact-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" >Contact</Link>
+          <Link to="contact-section" spy={true} smooth={true} offset={50} duration={500} className="nav-link" href="#" ><span>Contact</span></Link>
         </li>
       </ul>
       
     </div>
-  </div>
+  
   
 </nav>
 
