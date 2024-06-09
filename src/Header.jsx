58e88eddd18e4fd2e62bcf1188c053ed144fedf5
karim <karimisaac85@gmail.com>
2024-06-09 15:19:09 +0300

    style of navbar large screen added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 import {Link} from "react-scroll";
-
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
  function Heading() {
 //   useEffect(() => {
 //     window.addEventListener('scroll', function () {
@@ -16,6 +16,7 @@ import {Link} from "react-scroll";
 
    
       <a className="navbar-brand" href="#">Karim</a>
+      <DensityMediumIcon/>
       {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
        </button> */}
