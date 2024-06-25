5e4934e15df496c6d4aa6efa78b9d82b5e6f5899
karim <karimisaac85@gmail.com>
2024-06-25 15:35:15 +0300

    add style

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -5,35 +5,35 @@ function Resume() {
     return(
     <section className="" id="resume-section">
     	<div className="main-container">
-    		<div className="ٌHeader-Resume About-Container">
+    		<div className="ٌresume-middle">
           
-          	<h1 className="main-title-shadow Header-Resume">Resume</h1>
-            <h2 className="Header-Resume main-title main-font-title">Resume</h2>
+          	<h1 className="main-title-shadow resume-middle">Resume</h1>
+            <h2 className="resume-middle main-title main-font-title">Resume</h2>
             <p>Better late than never.</p>
           
         </div>
 
-    		<div className="Degrees-Resume">
-    			<div className="Degree-Container">
+    		<div className="resume-degrees">
+    			<div className="resume-degree-container">
     				<div className="">
-    					<span className="date">2002-2007</span>
+    					<span className="resume-date">2002-2007</span>
     					<h2>Bachelor Degree in Engineering</h2>
     					<span className="">AAST University</span>
     					<p className="">Bachelor Degree in Construction Engineering with an average GPA 3.69/4.00 "Excellent with honor".</p>
     				</div>
     			</div>
 
-    			<div className="Degree-Container">
+    			<div className="resume-degree-container">
                 <div className="">
-    					<span className="date">2009</span>
+    					<span className="resume-date">2009</span>
     					<h2>FE Exam</h2>
     					<span className="">NCEES USA</span>
     					<p className="">Passed the fundamental engineering exam from the NCEES USA.</p>
     				</div>
     			</div>
     		</div>
-
-    		<div className="Header-Resume">
+				
+    		<div className="resume-middle">
 				<StyledButton variant="contained">Download CV</StyledButton>
     				</div>
     		
