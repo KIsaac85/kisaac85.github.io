b7f95c38d94b7a9e6327ca493724028bfed6dfab
karim <karimisaac85@gmail.com>
2024-07-27 14:29:56 +0300

    add style to contact me

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -4,6 +4,9 @@ import EmailIcon from '@mui/icons-material/Email';
 import HomeIcon from '@mui/icons-material/Home';
 import emailjs from '@emailjs/browser';
 import {useForm} from "react-hook-form";
+import image1 from "./images/2.jpg"
+
+
 function ContactMe() {
 
   const {register,reset,handleSubmit,formState:{errors}}=useForm();
@@ -66,57 +69,59 @@ function ContactMe() {
               <h1 className="resume-middle main-title-shadow">Contact</h1>
               <h2 className="resume-middle main-title main-font-title">Contact Me</h2>
               <p className="resume-middle">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-            <div className="" style={{display:"flex",flexDirection:"row", justifyContent:"space-around"}}>
-            <div style={{width:200, display:"flex", flexDirection:"column", alignItems:"center"}}>
-              <div 
-              style={{backgroundColor:"grey"
-              ,borderRadius:50, width:100,height:100
-              , textAlign:"center" , margin:"auto"}}>
-                <HomeIcon/>
-                </div>
-              
-              <h3 className="">Address</h3>
-              <p>16 Abou Bakr elseddik street Dokki</p>
+            <div id="iconsContainer">
+            <div className="iconContainer">
+              <div className="imageContainer">
+                <HomeIcon className="imageIcon" />
+              </div>
+              <h3 >Address</h3>
+              <p id="textCentered">16 Abou Bakr elseddik street Dokki</p>
             </div>
             
           
-            <div className="">
-              <LocalPhoneIcon/>
-              <h3 className="mb-4">Contact Number</h3>
+            <div className="iconContainer">
+              <div className="imageContainer">
+              <LocalPhoneIcon className="imageIcon"/>
+              </div>
+              <h3 >Contact Number</h3>
               <p><a href="tel://+201224033791">+201224033791</a></p>
             </div>
 
-            <div className="">
-              <EmailIcon fontSize="medium" style={{color:"yellow"}}/>
-              <h3 className="mb-4">Email Address</h3>
-              <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+            <div className="iconContainer">
+              <div className="imageContainer">
+              <EmailIcon className="imageIcon"/>
+              </div>
+              <h3>Email Address</h3>
+              <p><a href="mailto:info@yoursite.com">karim_isaac@hotmail.com</a></p>
             </div>
             </div>
           </div>
   
-          <div className="row no-gutters block-9">
-            <div className="col-md-6 order-md-last d-flex">
-              <form  ref={form} action="#" className="bg-light p-4 p-md-5 contact-form">
-                <div className="form-group">
-                  <input {...register("fname",{required:true,minLength:{
+          <div className="" style={{display:"flex", flexDirection:"row"}}>
+          <div className="img" ><img src={image1}></img></div>
+            <div className="" style={{width:800}}>
+              <form style={{margin:50}} ref={form} action="#" className="">
+                <div className="form-group" >
+                  <input style={{width:600, margin:50, height:50}} {...register("fname",{required:true,minLength:{
                     message:"this is req"
                   }})}aria-invalid={errors.fname ? "true" : "false"}  type="text" className="form-control" placeholder={errors.fname?.message} />
                   <div>{errors.fname?.message}</div>
                 </div>
                 <div className="form-group">
-                  <input {...register("email",{required:true})} type="text" className="form-control" placeholder="Your Email" />
+                  <input style={{width:600, margin:50, height:50}} {...register("email",{required:true})} type="text" className="form-control" placeholder="Your Email" />
                 </div>
                 <div className="form-group">
-                  <input {...register("subject",{required:true})}   type="text" className="form-control" placeholder="Subject" />
+                  <input style={{width:600, margin:50, height:50}} {...register("subject",{required:true})}   type="text" className="form-control" placeholder="Subject" />
                 </div>
                 <div className="form-group">
-                  <textarea {...register("message",{required:true})}  id="" cols="30" rows="7" className="form-control" placeholder="Message" ></textarea>
+                  <textarea style={{width:600, margin:50, height:150 }} {...register("message",{required:true})}  id="" cols="30" rows="7" className="form-control" placeholder="Message" ></textarea>
                 </div>
                 <div className="form-group">
-                  <input type="submit" value="Send Message" onClick={handleSubmit(onSubmit)}  
-                  style={{ backgroundColor: isMouseOver ? "#ffa500" : "orange" }} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}className="btn btn-primary py-3 px-5"/>
+                  <input  type="submit" value="Send Message" onClick={handleSubmit(onSubmit)}  
+                  style={{ margin:50, backgroundColor: isMouseOver ? "yellow" : "orange" }} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}className="btn btn-primary py-3 px-5"/>
                 </div>
                 <div style={{
+                  
                   opacity: hiddenDiv ? "0" : "1",
                   transition: "all .5s",
                   visibility: hiddenDiv? "hidden" : "visible"}}>Message sent successfully!</div>
@@ -124,10 +129,7 @@ function ContactMe() {
               </form>
             
             </div>
-  
-            <div className="col-md-6 d-flex">
-                <div className="img" ></div>
-            </div>
+
           </div>
         </div>
       </section>
