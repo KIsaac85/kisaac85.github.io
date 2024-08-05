4392b0026aecf3c81a0a588161f02931940c9a80
Karim <karim_isaac@hotmail.com>
2024-08-05 21:38:29 +0300

    add style to home using fade from MUI

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,28 @@
-import React, { useState , useEffect} from 'react'
+import React from 'react'
 import image1 from "./images/2.jpg"
 import { Stack} from '@mui/material';
 import  StyledButton  from './js/ButtonStyle';
 import {Link} from "react-scroll";
 import Fade from '@mui/material/Fade';
 
-window.onfocus = function() {
-  console.log('Focused');
-  
-};
-
-window.onblur = function() {
-  console.log('Not focused');
-};
+window.onfocus = ()=> {location.reload();};
 
 
 function Home() {
-  const [role, setRole] = useState("Bim App Dev");
-  const [className, setClassName] = useState("fade");
-
   const [checked, setChecked] = React.useState(false);
 
   const handleChange = () => {
     setChecked((prev) => !prev);
+    
   };
-    const inter = setInterval(()=>{
-      
-      handleChange()
-    },5000)
+
+  const inter = setInterval(()=>{
+    
+    handleChange()
+  },5000)
 
 
- function changeHome() {
-   role ==="A Freelance Full Stack Web Developer"?setRole("Bim App Dev"):setRole("A Freelance Full Stack Web Developer");
-   
-   clearInterval(inter);
- };
-
- useEffect(()=>{},[role])
+ 
 
   return (
   
@@ -44,15 +30,14 @@ function Home() {
               
   <div  id="home">  
       <div>
-        <span>Hello!</span>
-      <h1>I'm <span>Karim Isaac</span></h1>
-      <Fade style={{position:"absolute"}} in={checked} timeout={2500}><h2>Bim App Dev</h2></Fade>
-      <Fade style={{position:'relative'}} in={!checked} timeout={2500}><h2 >A Freelance Full Stack Web Developer</h2></Fade>
-
+        
+      <Fade style={{position:"relative"}} in={checked}  timeout={1000}>
+      <div><span>Hello!</span>
+      <h1>I'm <span>Karim Isaac</span></h1><h2>Bim App Dev</h2></div></Fade>
+      <Fade style={{position:'relative',top:-220}} in={!checked} timeout={1000}>
+      <div><span>Hello!</span>
+      <h1>I'm <span>Karim Isaac</span></h1><h2 >A Freelance Full Stack Web Developer</h2></div></Fade>
       
-       
-       
-       
       <Stack spacing={2} direction={'row'}>
       <Link to="contact-section" spy={true} smooth={true} offset={50} duration={500}><StyledButton variant="contained">Hire Me</StyledButton></Link>
       <Link to="projects-section" spy={true} smooth={true} offset={50} duration={500}><StyledButton variant="contained">My works</StyledButton></Link> 
