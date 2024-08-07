2f843e20b6d077e42c4d2abba3ac3c0886631598
karim <karimisaac85@gmail.com>
2024-08-07 16:22:39 +0300

    modify style in home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import image1 from "./images/2.jpg"
+import image1 from "./images/2.jpg";
+import image2 from "./images/1.jpg";
 import { Stack} from '@mui/material';
 import  StyledButton  from './js/ButtonStyle';
 import {Link} from "react-scroll";
@@ -10,7 +11,6 @@ window.onfocus = ()=> {location.reload();};
 
 function Home() {
   const [checked, setChecked] = React.useState(false);
-
   const handleChange = () => {
     setChecked((prev) => !prev);
     
@@ -28,27 +28,26 @@ function Home() {
   
     
               
-  <div  id="home">  
-      <div>
+  <div  id="home-section">  
+      <div className='cony' style={{}}>
         
-      <Fade style={{position:"relative"}} in={checked}  timeout={1000}>
+      <Fade  in={checked}  timeout={2500}>
       <div><span>Hello!</span>
       <h1>I'm <span>Karim Isaac</span></h1><h2>Bim App Dev</h2></div></Fade>
-      <Fade style={{position:'relative',top:-220}} in={!checked} timeout={1000}>
+      <Fade className='home-relative-position'  in={!checked} timeout={2500}>
       <div><span>Hello!</span>
       <h1>I'm <span>Karim Isaac</span></h1><h2 >A Freelance Full Stack Web Developer</h2></div></Fade>
       
-      <Stack spacing={2} direction={'row'}>
+      <Stack className='home-relative-position'  spacing={2} direction={'row'}>
       <Link to="contact-section" spy={true} smooth={true} offset={50} duration={500}><StyledButton variant="contained">Hire Me</StyledButton></Link>
       <Link to="projects-section" spy={true} smooth={true} offset={50} duration={500}><StyledButton variant="contained">My works</StyledButton></Link> 
      
       </Stack>
          
        </div>
-      
-       <div style={{backgroundImage:`url(${image1}`}}>
-	          		
-       </div>
+      <Fade in={!checked}  className='' style={{position:"relative", left:480}} timeout={2500}><img src={`${image1}`} alt="personal photo"></img></Fade>
+      <Fade in={checked} className='' timeout={2500} ><img src={`${image2}`} alt="personal photo"></img></Fade>
+       
        
        </div>
 	 
