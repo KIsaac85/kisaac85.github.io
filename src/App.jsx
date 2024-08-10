47511260bbcc51489bfd0994abdf0249a25309d5
karim <karimisaac85@gmail.com>
2024-08-10 15:25:14 +0300

    add navbar from mui location needs to be adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 
 import AboutMe from './Aboutme.jsx'
 import Home from "./Home.jsx"
-import Heading from './Header.jsx'
+import Heading from './NavBar.jsx'
 import Resume from './Resume.jsx'
 import Skills from './Skills.jsx'
 import Projects from "./Projects.jsx"
 import ContactMe from './ContactMe.jsx'
 import Footer from './Footer.jsx'
 
+
 function App() {
  
 
   return (
 <div id="main-div">
+
+
     <Heading/>
+    
+
     <Home />
     <AboutMe/>
     <Resume/>
