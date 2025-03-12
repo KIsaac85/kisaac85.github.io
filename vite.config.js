fa35f5cf63a92c43081b148e0763bd4bd21e8278
Karim <karim_isaac@hotmail.com>
2025-03-12 12:08:30 +0200

    Update homepage URL

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Karim-Portfolio/', // Ensure this matches your repository namerm -rf node_modules/.cache/gh-pages
 })
